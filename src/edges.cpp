44016b3a4d911d03858e085fc57dd0812b3a080d
glazfu <952444278@qq.com>
2021-03-06 22:19:01 +0800

    finish tasks

--- src/edges.cpp
+++ src/edges.cpp
@@ -1,8 +1,31 @@
 #include "edges.h"
-
+#include <vector>
+#include <set>
 Eigen::MatrixXi edges(const Eigen::MatrixXi &F)
 {
   Eigen::MatrixXi E;
   // ADD YOUR CODE HERE
+  typedef std::pair<int,int> IJ;
+  std::vector<IJ> edge_pairs(F.size());
+  int cols = F.cols();
+  int rows = F.rows();
+  int offset = cols;
+#pragma omp parallel for default(none) shared(F,edge_pairs,rows,cols,offset)
+  for (int i = 0; i < rows; i++){
+    for(int j = 0; j < cols; j++) {
+      int s = F(i, j);
+      int e = F(i, (j+1)%cols);
+      if(s > e)
+        std::swap(s,e);
+      edge_pairs[i * offset + j] = IJ(s, e);
+    }
+  }
+  std::set<IJ> edge_set(edge_pairs.begin(),edge_pairs.end());
+  E = Eigen::MatrixXi(edge_set.size(),2);
+  int i = 0;
+  for(auto it: edge_set)
+  {
+    E.row(i++) = Eigen::Vector2i(it.first,it.second);
+  }
   return E;
 }
--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -1,8 +1,21 @@
 #include "euler_characteristic.h"
-
+#include "edges.h"
 int euler_characteristic(const Eigen::MatrixXi &F)
 {
   int Chi = 0;
   // ADD YOUR CODE HERE
+  int v_max_num = F.maxCoeff()+1;
+  Eigen::VectorXi v_exist = Eigen::VectorXi::Zero(v_max_num);
+
+#pragma omp parallel for default(none) shared(v_exist,v_max_num)
+  for(int i = 0; i < v_max_num; i++)
+  {
+    v_exist[i] = 1;
+  }
+  int e = edges(F).rows();
+  int f = F.rows();
+  int v = v_exist.sum();
+  Chi = v - e + f;
+
   return Chi;
 }
